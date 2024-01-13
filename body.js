d83e29689fcb2888540409926cc7fb6cb81bb79f
phani kumar <phanikumar21g@gmail.com>
2024-01-13 13:21:36 +0530

    added catlog body

--- body.js
+++ body.js
@@ -14,11 +14,23 @@ const Cardbody = () => {
   return (
     <div id="catalog">
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
 
 const Card = () => {
-  return <div id="card"></div>;
+  return (
+    <div id="card">
+      <img src={require("./utilities/food1.jpg")} alt="food" />
+      <div id="data">
+        <h3>Punjabi Dhaba</h3>
+        <h5>north india, punjabi</h5>
+      </div>
+    </div>
+  );
 };
 export default Body;
